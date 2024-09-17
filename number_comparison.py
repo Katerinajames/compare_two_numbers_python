bd8bad17884e5b2364104b59f66cb812339c7a9b
Katerina Paparizou <76427845+Katerinajames@users.noreply.github.com>
2024-09-17 20:45:59 +0300

    Add files via upload
     We enter two numbers from our keyboard and we compare them

---
+++ number_comparison.py
@@ -0,0 +1,8 @@
+
+number1 = int(input( "Please enter first integer: \n") )
+number2 = int(input( "Please enter second  integer: \n") )
+if number1 == number2:
+ print ("%d is equal to %d" % ( number1, number2 ))
+
+if number1 !=number2:
+	print ("%d is different from %d"% (number1 ,number2))
